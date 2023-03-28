a0b27c17808a2694236c50472a09f76e99784334
czyzix <czyzevsky@gmail.com>
2023-03-28 16:23:48 +0200

    minor change

--- src/components/product/product.js
+++ src/components/product/product.js
@@ -41,7 +41,7 @@ const Product = (props) => {
                 <div className="img-button-container">
                     <img
                         className="product-img"
-                        src={`productphotos/${imgSrc}.jpg`}
+                        src={`productphotos/${imgSrc}.JPG`}
                         alt={product.name}
                         onMouseOver={handleMouseOver}
                         onMouseOut={handleMouseOut}
